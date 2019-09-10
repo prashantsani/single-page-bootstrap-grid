47991cfad89382fa50ecefb6cc94107042e9f77e
Prashant Sani <contact@prashantsani.com>
2019-09-11 01:21:01 +0530

    adding Vanilla JS functionality to <WE CAN ORGANISE> section links

--- js/script.js
+++ js/script.js
@@ -11,4 +11,23 @@
 	d.getElementById('menu-open').addEventListener('click', function(){
 		d.getElementById('nav-primary').classList.toggle('active');
 	})
+
+	d.querySelectorAll('.block-link')
+	.forEach(function(el, index, array){
+		var _el = el;
+		el.addEventListener('click', function(e){
+			e.preventDefault();
+
+			// Remove Active Class on any sibling
+			var sibling = _el.parentNode.parentNode.querySelectorAll('.block-link.active')[0];
+			if(sibling){
+				sibling.classList.remove('active');
+			}	
+
+			// Add an active class to current element
+			_el.classList.toggle('active');
+
+			return false;
+		});
+	});
 })(window,document);
