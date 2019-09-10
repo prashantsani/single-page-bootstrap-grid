18c77ec81f13e393688832648a7f19f01d2e2dea
Prashant Sani <contact@prashantsani.com>
2019-09-11 01:28:55 +0530

    make <WE CAN ORGANISE> section links toggle on every click

--- js/script.js
+++ js/script.js
@@ -8,26 +8,39 @@
 */
 
 (function(w,d){
+	var active = 'active';
+
 	d.getElementById('menu-open').addEventListener('click', function(){
-		d.getElementById('nav-primary').classList.toggle('active');
+		d.getElementById('nav-primary').classList.toggle(active);
 	})
 
 	d.querySelectorAll('.block-link')
 	.forEach(function(el, index, array){
+
+		// cache this element + can be used in a closure function
 		var _el = el;
-		el.addEventListener('click', function(e){
+		
+		_el.addEventListener('click', function(e){
 			e.preventDefault();
+
+			// if the user has already clicked,
+			// in that case, we will remove the class
+			if(_el.classList.contains(active)){
+				_el.classList.remove(active);
+				return false
+			}
 
 			// Remove Active Class on any sibling
 			var sibling = _el.parentNode.parentNode.querySelectorAll('.block-link.active')[0];
 			if(sibling){
-				sibling.classList.remove('active');
+				sibling.classList.remove(active);
 			}	
 
 			// Add an active class to current element
-			_el.classList.toggle('active');
+			_el.classList.add(active);
 
 			return false;
 		});
 	});
+	
 })(window,document);
